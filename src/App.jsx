ec8c975e911a392c24747fd6002535882038e550
Angel <archenyel@gmail.com>
2025-06-23 18:26:13 -0600

    Se agrega varios forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import TipoRequisito from './components/TipoRequisito'
-import './App.css'
+import Categoria from './components/Categoria'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    
+      <h2>Tipo de Requisito</h2>
       <TipoRequisito />
-      <TipoRequisito />
-      <TipoRequisito />
+      <h2>Categoría</h2>
+      <Categoria />
     </>
   )
 }
